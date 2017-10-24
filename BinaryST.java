a69374c9306fcbb52d9d8a59636f999b83d7f2b6
十字街玻璃 <elithzhang@gmail.com>
2017-10-24 16:49:08 -0500

    add basises

---
+++ BinaryST.java
@@ -0,0 +1,74 @@
+// LEAVE THIS FILE IN THE DEFAULT PACKAGE
+//  (i.e., DO NOT add 'package cs311.pa1;' or similar)
+
+// DO NOT MODIFY THE EXISTING METHOD SIGNATURES
+//  (you may, however, add additional methods and fields)
+
+// DO NOT INCLUDE LIBRARIES OUTSIDE OF THE JAVA STANDARD LIBRARY
+//  (i.e., you may include java.util.ArrayList etc. here, but not junit, apache commons, google guava, etc.)
+
+
+public class BinaryST
+{
+	// member fields and methods
+	
+	public BinaryST()
+	{
+		// implementation
+	}
+	
+	public BinaryST(String[] s)
+	{
+		// implementation
+	}
+	
+	public int distinctSize()
+	{
+		// implementation
+	}
+	
+	public int size()
+	{
+		// implementation
+	}
+	
+	public int height()
+	{
+		// implementation
+	}
+	
+	public void add(String s)
+	{
+		// implementation
+	}
+	
+	public boolean search(String s)
+	{
+		// implementation
+	}
+	
+	public int frequency(String s)
+	{
+		// implementation
+	}
+	
+	public boolean remove(String s)
+	{
+		// implementation
+	}
+	
+	public String[] inOrder()
+	{
+		// implementation
+	}
+	
+	public String[] preOrder()
+	{
+		// implementation
+	}
+	
+	public int rankOf(String s)
+	{
+		// implementation
+	}
+}
